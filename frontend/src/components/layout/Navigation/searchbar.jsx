f9fbe6a606989f172254cc9be8b606ae50d69cec
Sir-Leon <eb3.61594.22@student.chuka.ac.ke>
2024-12-23 14:16:56 +0300

    First commit on windows dev
    Made minor changes to the navigation bar fixing flex issues on different displays

--- frontend/src/components/layout/Navigation/searchbar.jsx
+++ frontend/src/components/layout/Navigation/searchbar.jsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search..."
-        className="w-full h-8 sm:h-8 sm:w-64 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="w-full sm:w-200 h-8 sm:h-8 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       />
     </div>
   );
