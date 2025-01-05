e058ae80bb5a7d3d82c86da48f1249ed726d3849
Gunnah Leon Agandi <114517374+Sir-Leon1@users.noreply.github.com>
2025-01-05 19:32:49 +0300

    Update Footer.jsx
    Removed unused import,

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import AuthPage from "../loginReg/AuthForm.jsx";
 import SignIn from "../loginReg/SignIn.jsx";
 import AuthForm from "../loginReg/AuthForm.jsx";
-import TestComponent from "../../contexts/AuthContext.test.jsx";
 
 const Footer = ({isloggedin}) => {
   const [isSignUp, setIsSignUp] = React.useState(false);
