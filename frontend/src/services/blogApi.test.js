65ff6ddfec4fadfaca1b2d5f9890e16b9bcfb756
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2025-01-07 14:24:52 +0300

    Updating to blog editor

--- frontend/src/services/blogApi.test.js
+++ frontend/src/services/blogApi.test.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { createBlog, updateBlog, deleteBlog } from './blogApi';
+import { createBlog, updateBlog, deleteBlog, getSpecificBlogAuthor, specificBlog, getAuthorsBlogList, readingList, featuredPost, getpopularTags, getlatestArticles } from './blogApi';
 
 describe('blogApi', () => {
   describe('createPost', () => {
     it('creates a post successfully', async () => {
       const newPost = { title: 'New Post', content: 'New Content' };
-      const result = await createBlog(newPost);
-      expect(result).toHaveProperty('id');
-      expect(result.title).toBe(newPost.title);
-      expect(result.content).toBe(newPost.content);
+      const result = await createBlog({ title: 'New Post', content: 'New Content' }, "675e6ae47bbe53863df4c03f");
+      expect(result.data).toHaveProperty('id');
+      expect(result.data.title).toBe(newPost.title);
+      expect(result.data.content).toBe(newPost.content);
     });
 
     it('handles create post failure', async () => {
@@ -30,10 +30,15 @@ describe('blogApi', () => {
 
   describe('updatePost', () => {
     it('updates a post successfully', async () => {
-      const updatedPost = { id: 1, title: 'Updated Post', content: 'Updated Content' };
+      const updatedPost = {
+        id: "677bd2c12a17002edc3bfdcf",
+        authorId: "675e6ae47bbe53863df4c03f",
+        title: 'Updated Post',
+        content: 'Updated Content'
+      };
       const result = await updateBlog(updatedPost.id, updatedPost);
-      expect(result.title).toBe(updatedPost.title);
-      expect(result.content).toBe(updatedPost.content);
+      expect(result.data.title).toBe(updatedPost.title);
+      expect(result.data.content).toBe(updatedPost.content);
     });
 
     it('handles update post failure', async () => {
